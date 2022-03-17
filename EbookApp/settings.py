711f7aa99dc3e48da8b166c6463d2cbe8d48a919
Rupesh <rupesh.thapa2050@gmail.com>
2022-03-17 14:42:42 +0545

    update

--- EbookApp/settings.py
+++ EbookApp/settings.py
@@ -73,9 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EbookApp.wsgi.application'
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
